626f5b8ed49ab741d6489f52986a65a553c106e2
RipKip00765 <156421045+RipKip00765@users.noreply.github.com>
2024-12-13 14:19:07 +0100

    de

--- APiKeuzeOnderdeel/APiKeuzeOnderdeel/Form1.Designer.cs
+++ APiKeuzeOnderdeel/APiKeuzeOnderdeel/Form1.Designer.cs
@@ -36,7 +36,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1124, 527);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "5";
             this.ResumeLayout(false);
 
         }
